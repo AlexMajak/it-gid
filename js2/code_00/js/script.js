b79de9dd5889a7ff5c37c1c498fee9fda856f21d
Alexmajak <alexmajak@mail.ru>
2023-03-06 13:45:50 +0300

    add six march

---
+++ js2/code_00/js/script.js
@@ -0,0 +1,17 @@
+// let a = 6;
+// let b = 'Hello';
+
+// console.log(a);
+
+// let inputIn = document.querySelector('.input-in');
+// let button = document.querySelector('button');
+// let div = document.querySelector('.out');
+
+// button.onclick = function () {
+// 	console.log(inputIn.value);
+// 	div.innerHTML = inputIn.value;
+// 	inputIn.value = '';
+// }
+
+
+
---
+++ js2/code_01/js/script.js
@@ -0,0 +1,9 @@
+console.log("Alexandr");
+console.log(4);
+console.log('Добро ' + 'пожаловать' + ' на курс');
+// alert(2019);
+console.log(2019 - 200);
+document.getElementById('six').innerHTML = ('Hello World');
+document.getElementById('seven').innerHTML = (12 * 12);
+document.querySelector('.out-8').innerHTML = ('task-8');
+document.querySelector('span.out-8').innerHTML = ('everyone');
